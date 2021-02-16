920aaea25811d9bef6392a966096acc88afd585a
MelkdeSousa <melk.sousap@gmail.com>
2021-02-16 00:45:55 -0300

    remove some lines

--- configSW.js
+++ configSW.js
@@ -1,29 +1,11 @@
 'use strict'
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(() => console.log('sw registered'))
-    .catch(err => console.error('error registering sw', err))
+  navigator.serviceWorker.register('/sw.js')
 }
-
-let deferredPrompt
 
 window.addEventListener('beforeinstallprompt', eventBeforeInstallPrompt => {
   eventBeforeInstallPrompt.preventDefault()
 
   eventBeforeInstallPrompt.prompt()
-
-  eventBeforeInstallPrompt.userChoice.then(choiceResult => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted')
-    } else {
-      console.log('User dismissed')
-    }
-    deferredPrompt = null
-  })
-})
-
-window.addEventListener('appinstalled', _ => {
-  console.log('PWA was installed')
 })
