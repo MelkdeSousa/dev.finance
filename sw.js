4c79b4ed693cba507d8314177d08efb8f8849f47
MelkdeSousa <melk.sousap@gmail.com>
2021-02-18 19:29:34 -0300

    remove cache

--- sw.js
+++ sw.js
@@ -21,9 +21,7 @@ const resourcesToCache = [
 ]
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(resourcesToCache))
-  )
+  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll([])))
 })
 
 self.addEventListener('fetch', event => {
